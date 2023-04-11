094d647ea7f543afb8154b7100dcfc48e78311a1
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2023-04-11 12:41:27 +0100

    Refactored routes

--- src/utils/Routes.ts
+++ src/utils/Routes.ts
@@ -3,8 +3,12 @@ import Constants from "../common/Constants";
 import uaParser from "ua-parser-js";
 
 export default abstract class Routes {
-  public static async MovieAssistantDownload(app: Application) {
-    app.get("/movie-assistant", async (req: Request, res: Response) => {
+  private static async _downloadMethod(
+    app: Application,
+    path: string,
+    link: string
+  ): Promise<Application> {
+    return app.get(`/${link}`, async (req: Request, res: Response) => {
       try {
         const userAgent = req.headers["user-agent"];
 
@@ -21,7 +25,7 @@ export default abstract class Routes {
 
         res.status(200);
         res.download(
-          `./src/applications/movieAssistant/movie-assistant-${devTypeAndExtension.deviceTypeAb}-setup.${devTypeAndExtension.extension}`
+          `${path}-${devTypeAndExtension.deviceTypeAb}-setup.${devTypeAndExtension.extension}`
         );
       } catch (e) {
         let message = Constants.InternalServerError;
@@ -33,34 +37,18 @@ export default abstract class Routes {
       }
     });
   }
+  public static async MovieAssistantDownload(app: Application) {
+    this._downloadMethod(
+      app,
+      "./src/applications/movieAssistant/movie-assistant",
+      "movie-assistant"
+    );
+  }
   public static async FishLoggerDownload(app: Application) {
-    app.get("/fish-logger", async (req: Request, res: Response) => {
-      try {
-        const userAgent = req.headers["user-agent"];
-
-        const deviceType = uaParser(userAgent);
-
-        const devTypeAndExtension = Constants.DeviceTypesAndExtensions.find(
-          (x) => x.deviceType === deviceType.os.name
-        );
-
-        if (!devTypeAndExtension) throw new Error(Constants.InvalidDevice);
-
-        if (devTypeAndExtension.deviceType === "Mac os")
-          throw new Error(Constants.NoMacVersion);
-
-        res.status(200);
-        res.download(
-          `./src/applications/fish-logger/fish-logger-${devTypeAndExtension.deviceTypeAb}-setup.${devTypeAndExtension.extension}`
-        );
-      } catch (e) {
-        let message = Constants.InternalServerError;
-
-        if (e instanceof Error) message = e.message;
-
-        res.status(501);
-        res.send(message);
-      }
-    });
+    this._downloadMethod(
+      app,
+      "./src/applications/fish-logger/fish-logger",
+      "fish-logger"
+    );
   }
 }
